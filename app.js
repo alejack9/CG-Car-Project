204d6fba305513395b67c5423ba0651d51bbec33
Alessandro Giacche <alessandrogiacche@gmail.com>
2020-12-19 11:06:04 +0100

    added text canvas and inputs

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-import * as webglUtils from './js/libs/webgl-utils.js';
-import { Scene } from './js/scene.js';
+import * as webglUtils from "./js/libs/webgl-utils.js";
+import { Scene } from "./js/scene.js";
 
 (async function boot() {
-	const canvas = document.getElementById('gl-surface');
+    const canvas = document.getElementById("gl-surface");
+    const textCanvas = document.getElementById("text");
+    var ctx = textCanvas.getContext("2d");
+    webglUtils.resizeCanvasToDisplaySize(textCanvas);
 
-	const gl = webglUtils.getWebGLContext(canvas);
+    const gl = webglUtils.getWebGLContext(canvas);
 
-	const scene = new Scene(gl);
-	await scene.load();
-	scene.begin();
+    const scene = new Scene(gl, ctx);
+    await scene.load();
+    scene.begin();
 })();
